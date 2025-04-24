993aeab73cfcd4e9237d3f5109baea989187e450
higortab <higor.tabraga@gmail.com>
2025-04-24 16:53:19 -0300

    FEAT:  Começo Estilização

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,16 @@
 <template>
   <header>
     <ul>
-      <li>
+      <li class="pesquisa">
         <h3>IFbooks</h3>
-        <p><span>|</span></p>
+        <img src="../src/assets/Line 1.png" alt="">
         <p>
-          Apreço a <br />
+          Apreço a
           leitura
         </p>
         <div>
-          <input type="text" placeholder="Pesquisar" />
           <img src="../src/assets/lupa.png" alt="" />
+          <input type="text" placeholder="Pesquisar" />
         </div>
       </li>
       <li>
@@ -22,13 +22,13 @@
           <a href="">Envio</a>
           <a href="">Devoluções</a>
         </nav>
-        <nav>
+        <div class="icons">
           <RouterLink to="/carrinho">
-            <span class="fa-solid fa-cart-shopping" style="color: #63e6be"></span> carrinho
+            <span class="fa-solid fa-cart-shopping" style="color: #63e6be"></span>
           </RouterLink>
           <span class="fa-solid fa-heart" style="color: #63e6be"></span>
           <span class="fa-solid fa-user" style="color: #63e6be"></span>
-        </nav>
+        </div>
       </li>
     </ul>
   </header>
@@ -70,4 +70,71 @@
   </footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+  header {
+    margin: 2vw 8vw;
+  }
+  nav {
+    display: flex;
+    margin-left: 30%;
+  }
+
+  header ul {
+    display: flex;
+    justify-content: center;
+    justify-content: flex-start;
+  }
+
+  header li {
+    display: flex;
+
+  }
+  header h3 {
+    font-size: 160%;
+    text-align: center;
+  }
+  header p {
+    color: #27ae5f7e;
+    line-height: 120%;
+  }
+
+  header img {
+    margin: 0 1% 0 2%;
+    font-size: 45%;
+  }
+
+  header li div {
+    display: flex;
+  }
+
+  input {
+  background-color: #F1F1F1;
+  margin-left: 6%;
+  width: 30vw;
+  height: 120%;
+  color: #B8B8B8;
+  border: none;
+  padding: 0 0 0 2%;
+  }
+
+  header ul li:first-child div img {
+  margin-left: 28%;
+  margin-top: 0.7%;
+  position: absolute;
+  }
+
+  nav a {
+    margin-right: 30%;
+    font-size: 130%;
+    color: #7B7881;
+    justify-content: center;
+  }
+
+  div.icons {
+    margin-left: 120%;
+  }
+
+  div.icons span {
+    font-size: 150%;
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -121,5 +121,5 @@
 </template>
 
 <style scoped>
-
+  
 </style>
